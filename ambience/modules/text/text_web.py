73cc27584ce2702da7b2e4f46554e7e38e75363b
qwertyforce <qwertyforce@protonmail.com>
2022-01-28 15:51:30 +0300

    Update text_web.py

--- ambience/modules/text/text_web.py
+++ ambience/modules/text/text_web.py
@@ -16,33 +16,46 @@ from paddleocr import PaddleOCR
 ocr_ru = PaddleOCR(use_angle_cls=True, lang="ru",show_log = False)
 ocr_en = PaddleOCR(use_angle_cls=True, lang="en",show_log = False)
 
-conn = sqlite3.connect('ocr_text.db')
+conn = sqlite3.connect('ocr_text_ru_eng.db')
 
-from rapidfuzz import process, fuzz
-import re 
+
+
+from fonetika.metaphone import RussianMetaphone
+METAPHONE_RU=RussianMetaphone()
+
+from abydos import phonetic,distance
+METAPHONE_ENG=phonetic.Metaphone()
+
+from transliterate import translit
+# import rapidfuzz
+from rapidfuzz import string_metric
+import re
 # IMAGE_PATH = "./../../../public/images"
 # sub_index = faiss.IndexFlat(512, faiss.METRIC_L1)
 # index_id_map = faiss.IndexIDMap2(sub_index)
 
 IMG_ID_TXT_ARR={}
 
-def get_all_data():
+def get_all_data_iterator(arraysize=10000):
     cursor = conn.cursor()
     query = '''
-    SELECT id, text_arr
-    FROM ocr_text
-    '''
+        SELECT id, text_arr
+        FROM ocr_text
+        '''
     cursor.execute(query)
-    all_rows = cursor.fetchall()
-    return list(map(lambda el:{"image_id":el[0],"text_arr":el[1]},all_rows))
+    while True:
+        results = cursor.fetchmany(arraysize)
+        if not results:
+            break
+        yield results
 
 def init_index():
-    image_data=get_all_data()
-    print(f'Images in db: {len(image_data)}')
-    for el in tqdm(image_data):
-        text_arr=json.loads(el["text_arr"])
-        IMG_ID_TXT_ARR[el["image_id"]]=text_arr
-    print(IMG_ID_TXT_ARR[261764])
+    for batch in tqdm(get_all_data_iterator(10000)):
+        for el in batch:
+            text_arr=json.loads(el[1])
+            IMG_ID_TXT_ARR[el[0]]=text_arr
+    print(IMG_ID_TXT_ARR["312649"])
+    print(f"Data entries in index: {len(IMG_ID_TXT_ARR)}")
     print("Index is ready")
 
 def get_text_by_image_id(id):
@@ -110,12 +123,16 @@ class MyEncoder(json.JSONEncoder):
 def convert_to_words_arr(text_arr):
     words_arr=[]
     for entry in text_arr:
-        word=entry[1][0]
+        word=entry[1][0].lower()
         # words_arr.append(word)
         word = word.strip()
         words_split= word.split()
         words_arr.extend(words_split)
+    words_arr.append("".join(words_arr))
+    words_arr = [word for word in words_arr if len(word) > 3]
+    words_arr=list(set(words_arr))
     return words_arr
+
 # def sync_db():
 #     ids_in_db = set(get_all_ids())
 #     file_names = listdir(IMAGE_PATH)
@@ -145,14 +162,18 @@ def resize_img_to_threshold(img):
 def get_ocr_text(image_buffer):
     query_image = cv2.imdecode(read_img_file(image_buffer),cv2.IMREAD_GRAYSCALE)
     query_image = resize_img_to_threshold(query_image)
+    query_image = cv2.copyMakeBorder(query_image, 50, 50, 50, 50, cv2.BORDER_CONSTANT, value=[255,255,255])
+    # query_image = cv2.fastNlMeansDenoising(query_image, h=7, templateWindowSize=7, searchWindowSize=21)
 
     result_ru = ocr_ru.ocr(query_image, cls=True)
     result_en = ocr_en.ocr(query_image, cls=True)
     final_words = []
     for txt in result_ru:
+        coords_ru=str(txt[0])
         flag1 = False
         for _txt in result_en:
-            if str(txt[0]) == str(_txt[0]):
+            coords_en=str(_txt[0])
+            if coords_ru == coords_en:
                 flag1 = True
                 if txt[1][1] > _txt[1][1]:
                     final_words.append(txt)
@@ -163,113 +184,92 @@ def get_ocr_text(image_buffer):
             final_words.append(txt)
     # print(final_words)
     for txt in result_en:
+        coords_en=str(txt[0])
         flag1 = False
         for _txt in final_words:
-            if str(txt[0]) == str(_txt[0]):
+            coords=str(_txt[0])
+            if coords_en == coords:
                 flag1 = True
                 break
         if flag1 == False:
             final_words.append(txt)
-    print(final_words)
+    # print(final_words)
     return final_words
 
 
 def has_cyrillic(text):
     return bool(re.search('[а-яА-Я]', text))
 
-
-from fonetika.metaphone import RussianMetaphone
-METAPHONE_RU=RussianMetaphone()
-
-from abydos import phonetic,distance
-METAPHONE_ENG=phonetic.Metaphone()
-
-from transliterate import translit
-import rapidfuzz
+def cmp_string(algo,word1,word2):
+    if algo == "leven":
+        return string_metric.normalized_levenshtein(word1,word2,weights=(2,2,1))
+    if algo == "jaro_winkler":
+        return string_metric.jaro_winkler_similarity(word1, word2)
 
 def text_find_similar(text_arr):
     words_arr = convert_to_words_arr(text_arr)
-    # query_text_en="adidas"
-    # query_text_ru=translit(query_text_en,"ru",reversed=False)
-    # metaphone_ru=METAPHONE_RU.transform(query_text_ru)
-    # metaphone_eng=METAPHONE_ENG.encode(query_text_en)
-    similar=set()
+    print(words_arr)
+
+    similar_ids_set=set()
+    similar=[]
     for key,value in tqdm(IMG_ID_TXT_ARR.items()): #key == image_id
-        # print(image)
         for target_word in words_arr:
             target_word_is_cyrillic=has_cyrillic(target_word)
             db_words=convert_to_words_arr(value)
+            img_relevant_words=0
             for db_word in db_words:
                 w_ratio_metaphone_en=None
                 w_ratio_metaphone_ru=None
-                w_ratio_orig=None
-                if has_cyrillic(db_word) == target_word_is_cyrillic:
-                    w_ratio_orig=rapidfuzz.string_metric.normalized_levenshtein(target_word,db_word,weights=(2,2,1))
+                norm_levenstein_orig=None
+                if has_cyrillic(db_word) == target_word_is_cyrillic: #both target and db words are cyrillic or both are not cyrillic
+                    norm_levenstein_orig=cmp_string("leven", target_word,db_word)
                     if target_word_is_cyrillic==True:
                         target_word_metaphone_ru = METAPHONE_RU.transform(target_word)
                         db_word_metaphone_ru = METAPHONE_RU.transform(db_word)
-                        w_ratio_metaphone_ru=fuzz.WRatio(target_word_metaphone_ru,db_word_metaphone_ru)
+                        w_ratio_metaphone_ru=cmp_string("leven", target_word_metaphone_ru,db_word_metaphone_ru)
                     else:
-                        target_word_arr=target_word.strip().split()
-                        target_word_metaphone_eng=""
-                        for w in target_word_arr:
-                            target_word_metaphone_eng+=METAPHONE_ENG.encode(w)
-
-                        db_word_arr=db_word.strip().split()
-                        db_word_metaphone_eng=""
-                        for w in db_word_arr:
-                            db_word_metaphone_eng+=METAPHONE_ENG.encode(w)
-                        w_ratio_metaphone_en=fuzz.WRatio(target_word_metaphone_eng,db_word_metaphone_eng)
+                        target_word_metaphone_eng = METAPHONE_ENG.encode(target_word)
+                        db_word_metaphone_eng = METAPHONE_ENG.encode(db_word)
+                        w_ratio_metaphone_en=cmp_string("leven", target_word_metaphone_eng,db_word_metaphone_eng)
                 else:
-                    if target_word_is_cyrillic:
-                        w_ratio_orig1=rapidfuzz.string_metric.normalized_levenshtein(translit(target_word,"ru",reversed=True),db_word,weights=(2,2,1))
-                        w_ratio_orig2=rapidfuzz.string_metric.normalized_levenshtein(target_word,translit(db_word,"ru"),weights=(2,2,1))
-                        w_ratio_orig=max(w_ratio_orig1,w_ratio_orig2)
+                    if target_word_is_cyrillic: # if target word is cyrillic and db word is not cyrillic
+                        norm_levenstein_orig1=cmp_string("leven", translit(target_word,"ru",reversed=True),db_word)
+                        norm_levenstein_orig2=cmp_string("leven", target_word,translit(db_word,"ru"))
+                        norm_levenstein_orig=max(norm_levenstein_orig1,norm_levenstein_orig2)
 
                         target_word_metaphone_ru = METAPHONE_RU.transform(target_word)
                         db_word_metaphone_ru = METAPHONE_RU.transform(translit(db_word,"ru"))
-                        w_ratio_metaphone_ru=fuzz.WRatio(target_word_metaphone_ru,db_word_metaphone_ru)
+                        w_ratio_metaphone_ru=cmp_string("leven", target_word_metaphone_ru,db_word_metaphone_ru)
+                        if w_ratio_metaphone_ru>70:
+                            print("====")
+                            print(target_word_metaphone_ru)
+                            print(db_word)
+                            print(db_word_metaphone_ru)
+                            print("====")
 
-                    else:
-                        w_ratio_orig1=rapidfuzz.string_metric.normalized_levenshtein(translit(target_word,"ru"),db_word,weights=(2,2,1))
-                        w_ratio_orig2=rapidfuzz.string_metric.normalized_levenshtein(target_word,translit(db_word,"ru",reversed=True),weights=(2,2,1))
-                        w_ratio_orig=max(w_ratio_orig1,w_ratio_orig2)
-
-                        target_word_arr=target_word.strip().split()
-                        target_word_metaphone_eng=""
-                        for w in target_word_arr:
-                            target_word_metaphone_eng+=METAPHONE_ENG.encode(w)
-
-                        db_word_arr=translit(target_word,"ru",reversed=True).strip().split()
-                        db_word_metaphone_eng=""
-                        for w in db_word_arr:
-                            db_word_metaphone_eng+=METAPHONE_ENG.encode(w)
-                        w_ratio_metaphone_en=fuzz.WRatio(target_word_metaphone_eng,db_word_metaphone_eng)
-                
-                if w_ratio_orig>70:
-                    similar.add(key)
+                    else:   # if target word is not cyrillic and db word is cyrillic
+                        norm_levenstein_orig1=cmp_string("leven", translit(target_word,"ru"),db_word)
+                        norm_levenstein_orig2=cmp_string("leven", target_word,translit(db_word,"ru",reversed=True))
+                        norm_levenstein_orig=max(norm_levenstein_orig1,norm_levenstein_orig2)
+                        target_word_metaphone_eng = METAPHONE_ENG.encode(target_word)
+                        db_word_metaphone_eng = METAPHONE_ENG.encode(db_word)
+                        w_ratio_metaphone_en=cmp_string("leven", target_word_metaphone_eng,db_word_metaphone_eng)
+                if (w_ratio_metaphone_en and w_ratio_metaphone_en == 100) or (w_ratio_metaphone_ru and w_ratio_metaphone_ru == 100) or (norm_levenstein_orig and norm_levenstein_orig > 70) and key not in similar_ids_set:
+                    img_relevant_words+=1
+                    similar_ids_set.add(key)
+                    similar.append({
+                        "image_id": key,
+                        "score": {
+                            "w_ratio_metaphone_en": w_ratio_metaphone_en,
+                            "w_ratio_metaphone_ru": w_ratio_metaphone_ru,
+                            "norm_levenstein_orig": norm_levenstein_orig
+                        }
+                    })
                 # print(data)
-        
-            # if len(data[1][0])>=4:
-            #     # rt=process.fuzz.token_set_ratio(query_text,data[1][0],force_ascii=False)
-            #     if has_cyrillic(data[1][0]):
-            #         temp_metaph=METAPHONE_RU.transform(data[1][0])
-            #         dist=distance.levenshtein(metaphone_ru,temp_metaph)
-            #         if dist<=1:
-            #             print()
-            #             print(dist,data[1][0])
-            #             print(key,value)
-            #             print()
-            #     else:
-            #         continue
-            #         temp_metaph=METAPHONE_ENG.encode(data[1][0])
-            #         dist=distance.levenshtein(metaphone_eng,temp_metaph)
-            #         if dist<=1:
-            #             print()
-            #             print(dist,data[1][0])
-            #             print(key,value)
-            #             print()
     print(similar)
+    return similar
+
+
 app = FastAPI()
 @app.get("/")
 async def read_root():
@@ -303,8 +303,10 @@ async def text_get_similar_images_by_id_handler(item: Item_image_id):
 @app.post("/text_get_similar_images_by_image_buffer")
 async def text_get_similar_images_by_image_buffer_handler(image: bytes = File(...)):
     ocr_text_arr = get_ocr_text(image)
+    # print(convert_to_words_arr(ocr_text_arr))
     if len(ocr_text_arr) == 0:
         return {"error":"not text detected"}
+    # return []
     similar = text_find_similar(ocr_text_arr)
     return similar
 
